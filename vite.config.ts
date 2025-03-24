28428058e67f2501356f615ff630a65c1ea6661e
Tamia Pulfer <tamia.pulfer@t-online.de>
2025-03-24 12:02:40 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html', // Ensure Vite knows where to start
+    },
   },
 });
